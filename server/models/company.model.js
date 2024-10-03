8b6dc230c32eb0f2daa461e887dc52741ba21a9f
Sunnyk16 <kumarsunny1246@gmail.com>
2024-10-03 16:43:21 +0530

    Refactor company model to remove required field for userId

--- server/models/company.model.js
+++ server/models/company.model.js
@@ -28,8 +28,8 @@ const companySchema = new mongoose.Schema({
     },
     userId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
+        ref: "User",
+        // required: true,
     },
     jobType: [
         {
